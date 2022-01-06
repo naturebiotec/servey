21019626673567405cdc04742fedb328c27363c3
Naturebiotec <94585228+naturebiotec@users.noreply.github.com>
2022-01-06 16:34:20 +0700

    add images servery

--- servey_fontend2/index.php
+++ servey_fontend2/index.php
@@ -19,8 +19,8 @@
         <div class="row justify-content-center">
             <div class="col-11 col-sm-9 col-md-7 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2">
                 <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                    <h2 id="heading">Test System Servey</h2>
-                    <p>Fill all form field to go to next step</p>
+                    <!-- <h2 id="heading">Test System Servey</h2> -->
+                   <img src="./asset/img/1040x1040_AW_แบบสอบถาม-02.png" alt="" style="border-radius: 10px;">
                     <form id="msform"  name="form" id="form" method="get" action="#" class="needs-validation" novalidate>
                         <div class="form-group">
                             <!-- progressbar -->
