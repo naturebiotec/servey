d8f60b4f021243302e2f7c40d7c31bd9055bc473
Naturebiotec <94585228+naturebiotec@users.noreply.github.com>
2022-01-13 16:40:42 +0700

    conditions

--- servey_fontend2/conditions.php
+++ servey_fontend2/conditions.php
@@ -5,18 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>conditions</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <p>
-        ข้อกำหนดและนโยบายรักษาความเป็นส่วนตัวของข้อมูลลูกค้า
-    บริษัท เนเจอร์ไบโอเทค จำกัด คำนึงถึงสุขภาพและความปลอดภัยเป็นหลัก ซึ่งเป็นจุดเริ่มต้นที่นำความต้องการของลูกค้าไปพัฒนาและมอบประสบการณ์ที่ดีในครั้งต่อไป รวมทั้งการปกป้องข้อมูลส่วนตัวของลูกค้าที่ใช้บริการกับทางบริษัท โดยยึดหลักนโยบายความเป็นส่วนตัว ภายใต้กฏหมาย พระราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล พ.ศ. 2562
-    </p>
-    <p>
-        ลูกค้ายอมรับ ให้ทางการตลาดนำข้อมูลที่กรอกฟอร์มแบบประเมิน เพื่อให้ทางบริษัททำการวิเคราะห์ข้อมูล เพื่อนำไปประเมินและวิเคราะห์ ผลิตภัธฑ์ต่างๆ  
-    </p>
-    <p>
-        ลูกค้ายอมรับ ให้ทางการบริษัทติดต่อสอบถามเพิ่มเติม 
-    </p>
+    <div class="container-fluid">
+            <div class="card shadow md-auto"style="margin-top:15px;">
+                    <div class="card-body">
+                        <p style="color:green;text-transform: uppercase;">บริษัท เนเจอร์ไบโอเทค จำกัด</p>
+                        <hr>
+                        <h6>
+                            คำนึงถึงสุขภาพและความปลอดภัยเป็นหลัก ซึ่งเป็นจุดเริ่มต้นที่นำความต้องการของลูกค้าไปพัฒนา และมอบประสบการณ์ที่ดีในครั้งต่อไป <br> รวมทั้งการปกป้องข้อมูลส่วนตัวของลูกค้าที่ใช้บริการกับทางบริษัท โดยยึดหลักนโยบายความเป็นส่วนตัว ภายใต้กฏหมาย พระราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล พ.ศ. 2562
+                        </h6>
+                        <h6>ลูกค้ายอมรับ ให้ทางการตลาดนำข้อมูลที่กรอกฟอร์มแบบประเมิน เพื่อให้ทางบริษัททำการวิเคราะห์ข้อมูล <br> เพื่อนำไปประเมินและวิเคราะห์ ผลิตภัณฑ์</h6>
+                        <button  type="submit" class="btn btn-success btn-block" onclick="goBack()">กลับ</button>
+                    </div>
+            </div>
+        </div>
 </body>
 </html>
+<script>
+ function goBack() {
+    window.history.back();
+    }
+</script>
