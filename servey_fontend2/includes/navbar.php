90b078de1f1c31949071b7779a34a08888f3a0e0
Naturebiotec <94585228+naturebiotec@users.noreply.github.com>
2022-01-17 17:16:44 +0700

    Navbar

--- servey_fontend2/includes/navbar.php
+++ servey_fontend2/includes/navbar.php
@@ -1,13 +1,105 @@
-<!-- Side bar -->
-<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
-    <!-- Side bar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-        <div class="sidebar-brand-icon">
-            <img src="#" alt="logo" width="70px" height="70px">
-        </div>
-        <div class="sidebar-brand-text mx-3">Nature <sub>Biotec</sub></div>
-    </a>
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-    <!-- ต่อ พน. -->
+<!-- login php -->
+   <!-- Sidebar -->
+   <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+  <div class="sidebar-brand-icon">
+       <img src="./img/NB-Logo-3.png" alt="logo" width="70px" height="70px">
+  </div>
+  <div class="sidebar-brand-text mx-3">Nature <sub>Biotec</sub></div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard 
+<li class="nav-item active">
+  <a class="nav-link" href="index.php">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <span>Dashboard</span></a>
+</li>  -->
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Interface
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+    <i class="fa fa-users"></i>
+    <span>Report</span>
+  </a>
+  <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <a class="collapse-item" href="#">ข้อมูลลูกค้า</a>
+      <a class="collapse-item" href="#">สิทธิประโยชน์</a>
+      <a class="collapse-item" href="#">ประวัติการใช้สิทธิ์</a>
+  </div> 
+</li> 
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
 </ul>
+<!-- End of Sidebar -->
+
+   <!-- Connext TO Page Write Codeing to WebDeshbord new  -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content --> 
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Search " aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+            </li>
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
