4d698c48da58da0b951da59f14a5e925c45acde4
Naturebiotec <94585228+naturebiotec@users.noreply.github.com>
2022-01-17 17:16:23 +0700

    footer

--- servey_fontend2/includes/footer.php
+++ servey_fontend2/includes/footer.php
@@ -1,9 +1,12 @@
+
+
    <!-- Footer -->
    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>Nature Biotec @2020 </span>
           </div>
-       
+        </div>
       </footer>
       <!-- End of Footer -->
 
